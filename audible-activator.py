8790d9fdd6786bb88310b10a9cdf3840abdc59c4
seanth <seanth@gmail.com>
2022-07-14 17:41:22 -0500

    Update audible-activator.py
    include fixes from https://github.com/inAudible-NG/audible-activator/pull/55

--- audible-activator.py
+++ audible-activator.py
@@ -99,9 +99,13 @@ def fetch_activation_bytes(username, password, options):
         print("[!] Running in DEBUG mode. You will need to login in a semi-automatic way, wait for the login screen to show up ;)")
         time.sleep(32)
     else:
-        search_box = driver.find_element_by_id('ap_email')
+        #https://github.com/inAudible-NG/audible-activator/pull/55
+        #search_box = driver.find_element_by_id('ap_email')
+        search_box = driver.find_element('id','ap_email')
         search_box.send_keys(username)
-        search_box = driver.find_element_by_id('ap_password')
+        #https://github.com/inAudible-NG/audible-activator/pull/55
+        #search_box = driver.find_element_by_id('ap_password')
+        search_box = driver.find_element('id','ap_password')
         search_box.send_keys(password)
         search_box.submit()
         time.sleep(2)  # give the page some time to load
